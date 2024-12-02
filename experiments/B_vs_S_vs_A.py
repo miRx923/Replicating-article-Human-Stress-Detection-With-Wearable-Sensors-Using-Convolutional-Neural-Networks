60f5241cfd3ce821810fdc1b870069535ad7e002
miRx923 <94078303+miRx923@users.noreply.github.com>
2024-12-02 09:29:27 +0100

    Add files via upload

--- experiments/B_vs_S_vs_A.py
+++ experiments/B_vs_S_vs_A.py
@@ -1,12 +1,16 @@
 # File for running (B vs S vs A) experiments
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "src")))
+
 from model.train_n_test import experiments
 
 
 
 # Run training and all experiments at once
-data_files = ["./Data/WESAD_final/fft_normalized.pkl", 
-              "./Data/WESAD_final/fft_cr_normalized.pkl", 
-              "./Data/WESAD_final/fft_cr_cqt_normalized.pkl"
+data_files = ["data/processed/fft_normalized.pkl", 
+              "data/processed/fft_cr_normalized.pkl", 
+              "data/processed/fft_cr_cqt_normalized.pkl"
              ]
 
 tasks = ["B_vs_S_vs_A"]
--- experiments/S_vs_NS.py
+++ experiments/S_vs_NS.py
@@ -1,12 +1,16 @@
 # File for running (S vs NS) experiments
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), "src")))
+
 from model.train_n_test import experiments
 
 
 
 # Run training and all experiments at once
-data_files = ["./Data/WESAD_final/fft_normalized.pkl", 
-              "./Data/WESAD_final/fft_cr_normalized.pkl", 
-              "./Data/WESAD_final/fft_cr_cqt_normalized.pkl"
+data_files = ["data/processed/fft_normalized.pkl", 
+              "data/processed/fft_cr_normalized.pkl", 
+              "data/processed/fft_cr_cqt_normalized.pkl"
              ]
 
 tasks = ["S_vs_NS"]
